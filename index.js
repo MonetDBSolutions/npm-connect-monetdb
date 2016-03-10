8c1d7d66869d511b663ef41170e3cea8e1c4bf1d
R. Cijvat <robin.cijvat@monetdbsolutions.com>
2016-03-10 07:41:31 +0100

    More defensive code

--- index.js
+++ index.js
@@ -1,65 +1,66 @@
 
 module.exports = function(session) {
 
-	function MDBSessStore(db) {
-		if(!db.query) {
+    function MDBSessStore(db) {
+        if(!db.query) {
             throw new Error("MDBSessStore needs as its first argument either a MonetDBConnection object or a MonetDBPool object.");
         }
         this.db = db;
-	}
+    }
 
-	MDBSessStore.prototype = new session.Store();
-	MDBSessStore.prototype.constructor = MDBSessStore;
+    MDBSessStore.prototype = new session.Store();
+    MDBSessStore.prototype.constructor = MDBSessStore;
 
 
-	MDBSessStore.prototype.get = function(sid, fn) {
-		var self = this;
-		var now = Math.round(Date.now() / 1000);
-		if(Math.random() < 0.01) {
-			self.db.query("DELETE FROM session WHERE expire < ?", [now]);
-		}
-		self.db.query("SELECT sess FROM session WHERE sid = ? AND expire >= ?", [sid, now], false).then(function(result) {
+    MDBSessStore.prototype.get = function(sid, fn) {
+        var self = this;
+        var now = Math.round(Date.now() / 1000);
+        if(Math.random() < 0.01) {
+            self.db.query("DELETE FROM session WHERE expire < ?", [now]);
+        }
+        self.db.query("SELECT sess FROM session WHERE sid = ? AND expire >= ?", [sid, now], false).then(function(result) {
             if (!result.data || !result.data[0] || !result.data[0][0]) {
                 return fn && fn(null);
             }
-            return fn && fn(null, JSON.parse(result.data[0][0]));
+            var parsedSess = JSON.parse(result.data[0][0].replace("\n", "\\n", "\r", "\\r"));
+            return fn && fn(null, parsedSess);
         }, function(err) {
             fn && fn(err);
         });
-	};
+    };
 
-	MDBSessStore.prototype.set = function(sid, sess, fn) {
-		var self = this;
-		var maxAge = sess.cookie.maxAge ? sess.cookie.maxAge : 24 * 3600;
+    MDBSessStore.prototype.set = function(sid, sess, fn) {
+        var self = this;
+        var maxAge = sess.cookie.maxAge ? sess.cookie.maxAge : 24 * 3600;
 
-		self.db.query("SELECT * FROM session WHERE sid = ?", [sid]).then(function(result) {
+        self.db.query("SELECT * FROM session WHERE sid = ?", [sid]).then(function(result) {
             var stringifiedSess = JSON.stringify(sess)
                 .replace("\n", "\\n")
                 .replace("\r", "\\r");
-			if(result.rows == 0) {
-				return self.db.query(
+            if(result.rows == 0) {
+                return self.db.query(
                     "INSERT INTO session (sid, sess, expire) VALUES (?, ?, ?)",
-					[sid, stringifiedSess, Math.round(Date.now() / 1000) + maxAge]
+                    [sid, stringifiedSess, Math.round(Date.now() / 1000) + maxAge]
                 );
-			}
+            }
             return self.db.query(
                 "UPDATE session SET sess = ? WHERE sid = ?",
                 [stringifiedSess, sid]
             );
-		}).then(function() {
+        }).then(function() {
             fn && fn(null);
         }).fail(function(err) {
             fn && fn(err);
         });
-	};
+    };
 
-	MDBSessStore.prototype.destroy = function(sid, fn) {
-		this.conn.query("DELETE FROM session WHERE sid = ?", [sid]).then(function() {
+    MDBSessStore.prototype.destroy = function(sid, fn) {
+        this.conn.query("DELETE FROM session WHERE sid = ?", [sid]).then(function() {
             fn && fn();
         }).fail(function(err) {
             fn && fn(err);
         });
-	};
+    };
 
-	return MDBSessStore;
+    return MDBSessStore;
 };
